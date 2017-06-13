4fa2e42499a961a7b4bcca219087fec6c33a355b
Jared Hanson <jaredhanson@gmail.com>
2017-06-13 14:25:18 -0700

    Cleanup.

--- app/jwt/unseal.js
+++ app/jwt/unseal.js
@@ -2,22 +2,9 @@ exports = module.exports = function(key) {
   // Load modules.
   var tokens = require('tokens');
   
-  function keying(iss, aud, header, cb) {
-    console.log('SAT DECODE KEYING....');
-    console.log(iss);
-    console.log(aud);
-    console.log(header);
-    
-    // TODO: Port this to use keyring
-    //keyring.find(options, cb);
-    
-    return cb(null, 'secret-foo-bar-asdfadfasfasdfaeafdasfsf');
-  }
-  
   return tokens.jwt.unseal(key);
 };
 
-//exports['@require'] = [ '../_internals/keying' ];
-exports['@require'] = [ '../common/key' ];
 exports['@implements'] = 'http://i.bixbyjs.org/tokens/unsealFunc';
 exports['@type'] = 'application/jwt';
+exports['@require'] = [ '../common/key' ];
