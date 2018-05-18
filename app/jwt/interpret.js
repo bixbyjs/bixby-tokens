45af0351adee683380adf9bef2e3b2edc46a0999
Jared Hanson <jaredhanson@gmail.com>
2018-05-18 07:00:41 -0700

    Remove dead code.

--- app/jwt/interpret.js
+++
@@ -1,9 +0,0 @@
-exports = module.exports = function() {
-  // Load modules.
-  var tokens = require('tokens');
-  
-  return tokens.jwt.interpret();
-};
-
-exports['@implements'] = 'http://i.bixbyjs.org/tokens/interpretClaimsFunc';
-exports['@dialect'] = 'urn:ietf:params:oauth:token-type:jwt';
--- app/jwt/translate.js
+++
@@ -1,9 +0,0 @@
-exports = module.exports = function() {
-  // Load modules.
-  var tokens = require('tokens');
-  
-  return tokens.jwt.translate();
-};
-
-exports['@implements'] = 'http://i.bixbyjs.org/tokens/translateContextFunc';
-exports['@dialect'] = 'urn:ietf:params:oauth:token-type:jwt';
